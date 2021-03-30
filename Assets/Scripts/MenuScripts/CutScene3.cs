65785ec830fa9d7a9fc3593ccdbd94f687bae3d2
CalebWalls <calebwalls@gmail.com>
2021-03-30 12:42:36 -0500

    added cut scene prototype. There are 3 cut scenes each will bring you to the next and the third one will bring you to level1

--- Assets/Scripts/MenuScripts/CutScene3.cs
+++ Assets/Scripts/MenuScripts/CutScene3.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class CutScene : MonoBehaviour
+public class CutScene3 : MonoBehaviour
 {
-    public void Continue(){
-        SceneManager.LoadScene(6);
+    public void Continue()
+    {
+        SceneManager.LoadScene(5);
     }
 
     public void QuitToMainMenu()
