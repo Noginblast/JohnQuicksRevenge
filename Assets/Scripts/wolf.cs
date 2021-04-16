d3cda3a06de92ab0d0a5a6840d89da1c7f5508a6
Cade M Forbes <cadeforbes1@gmail.com>
2021-04-15 22:43:47 -0500

    Adjusted jump glitch. Created wolf enemy prefab.

--- Assets/Scripts/wolf.cs
+++ Assets/Scripts/wolf.cs
@@ -4,16 +4,55 @@ using UnityEngine;
 
 public class wolf : MonoBehaviour
 {
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    private float leftCap;
+    private float rightCap;
+    [SerializeField] private float range = 10;
+
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private LayerMask ground;
+
+    private Collider2D coll;
+    private Rigidbody2D rb;
+    private float[] scale = new float[2];
+
+
+
+    private bool facingLeft = false;
+
+    private void Start(){
+        coll = GetComponent<Collider2D>();
+        rb = GetComponent<Rigidbody2D>();
+        scale[0] = transform.localScale.x;
+        scale[1] = transform.localScale.y;
+        leftCap = transform.position.x - range/2;
+        rightCap = transform.position.x + range/2;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(facingLeft){
+            if(transform.position.x > leftCap){
+                if(transform.localScale.x != scale[0]){
+                    transform.localScale = new Vector3(scale[0],scale[1]);
+                    rb.velocity = new Vector2(-speed,0);
+                }
+            }
+            else{
+                facingLeft = false;
+            }
+        }
+        else{
+            if(transform.position.x < rightCap){
+                if(transform.localScale.x != -scale[0]){
+                    transform.localScale = new Vector3(-scale[0],scale[1]);
+                    rb.velocity = new Vector2(speed,0);
+                }
+            }
+            else{
+                facingLeft = true;
+            }
+        }
     }
 }
